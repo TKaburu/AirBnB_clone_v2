49575827d1dea6154d2d9b79808fbd1a6ec7e0ed
TKaburu <traceymwendwa@gmail.com>
2024-01-31 09:55:18 -0800

    Feat: Prepare webservers for deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Set web servers for deployment
+
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create folders
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+# Create HTML file
+echo "
+        <html>
+                <head>
+                </head>
+                <body>
+                        Holberton School
+                </body>
+        </html>
+" >> /data/web_static/releases/test/index.html
+
+# Symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# New Nginx configuration
+echo "server {
+    listen 80;
+    server_name _;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+
+}
+
+" >> /etc/nginx/sites-available/default
+
+# Start Nginx
+sudo service nginx start
