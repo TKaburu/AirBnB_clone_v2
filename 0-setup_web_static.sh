97bc0e4dcc39b654477e9f526a1ca622fbfbea23
TKaburu <traceymwendwa@gmail.com>
2024-02-14 04:27:18 -0800

    Fix: echo command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,31 @@
 #!/usr/bin/env bash
-# Set web servers for deployment
+# sets up web servers for the deployment
 
 # Install Nginx if not already installed
 sudo apt-get update
-sudo apt-get -y install nginx
+sudo apt-get install -y nginx
 
 # Create folders
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create HTML file
-sudo echo "
-        <html>
-                <head>
-                </head>
-                <body>
-                        Holberton School
-                </body>
-        </html>
-" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 
 # Symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
 # New Nginx configuration
-sudo echo "
-server {
-    listen 80;
-    server_name _;
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
-}
-
-"  | sudo tee /etc/nginx/sites-available/default
-
-# Start Nginx
-sudo service nginx start
+# Restart Nginx to apply changes
+sudo service nginx restart
