b70b7e973243584b75034eb7504097ecd6d03669
TKaburu <traceymwendwa@gmail.com>
2024-02-15 22:37:46 -0800

    Fix: Added documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """
+    This function displays “Hello HBNB!”
+    """
+
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000))
