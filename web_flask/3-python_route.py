e60f043c50b9c76066ac3b3da1968ce392a7d3d9
TKaburu <traceymwendwa@gmail.com>
2024-02-16 11:16:46 +0300

    Feat: routing for /python and /python/<text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+A script that starts a flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    This function displays “Hello HBNB!”
+    """
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This function displays “HBNB”
+    """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+
+    text = text.replace("_", " ")
+
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is(text):
+    """
+    display “Python ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+
+    text = text.replace("_", " ")
+
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
