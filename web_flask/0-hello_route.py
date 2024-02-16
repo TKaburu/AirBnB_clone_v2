9864cfe07064855ea2eb99b646976a46d828da12
TKaburu <traceymwendwa@gmail.com>
2024-02-16 09:07:59 +0300

    Feat: Route "/'" displays “Hello HBNB!”

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# A script that starts a flask app and / returns “Hello HBNB!”
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
